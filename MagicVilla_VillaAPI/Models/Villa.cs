9457c650ed30ac007e5a48db6946baac0e5344d6
Amita Tiwari <at224217@gmail.com>
2023-04-24 11:45:21 +0530

    second commit

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+       public int Id { get; set; }
+       public String Name { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
